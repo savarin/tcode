cb30caed04123e5dd62db9bd3c649b4c2d97abbc
Ezzeri Esa <esa@squareup.com>
2018-01-06 09:43:03 -0800

    added unit decode

--- tcode.py
+++ tcode.py
@@ -13,3 +13,37 @@ def encode_unit(string):
     elif simple:
         return '+' + result
     return str(i + 1) + result
+
+
+def decode_unit(string):
+    digits = set([str(i) for i in xrange(10)])
+    result = ''
+    length = ''
+    bulk = False
+
+    for i, char in enumerate(string):
+        if not i and char in digits:
+            bulk = True
+            length += char
+            continue
+        elif bulk and char in digits:
+            length += char
+            continue
+        elif bulk and char not in digits:
+            bulk = False
+
+        result += char
+
+    assert result[-1] == '\n'
+
+    if result[0] == '~':
+        assert i == 1
+        result = result[1:-1]
+    elif result[0] == '+':
+        result = result[1:-1]
+    else:
+        assert length
+        assert len(result) == int(length) + 1
+        result = result[:-1]
+
+    return [result, i]
--- test_tcode.py
+++ test_tcode.py
@@ -5,3 +5,8 @@ def test_encode_unit():
     assert tcode.encode_unit('foo') == '+foo\n'
     assert tcode.encode_unit('') == '~\n'
     assert tcode.encode_unit('foo\n') == '4foo\n\n'
+
+def test_decode_unit():
+    assert tcode.decode_unit('+foo\n') == ['foo', 4]
+    assert tcode.decode_unit('~\n') == ['', 1]
+    assert tcode.decode_unit('4foo\n\n') == ['foo\n', 5]
